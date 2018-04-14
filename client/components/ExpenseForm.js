77f9d10aeb5435d6bcf3c2361a85cf33e21871b9
Vesna Tan <tanvezi@gmail.com>
2018-04-13 22:49:26 -0400

    refactor: cleaned up code in expenseform.js

--- client/components/ExpenseForm.js
+++ client/components/ExpenseForm.js
@@ -1,46 +1,6 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 
-
-// class ExpenseForm extends Component {
-//     constructor(){
-//         super()
-//         this.state = {
-//             item: '',
-//             price: 0.00
-//         }
-
-//         this.onSubmit = this.onSubmit.bind(this)
-//         this.onChange = this.onChange.bind(this)
-//     }
-
-//     onChange(event){
-//         this.setState({ [event.target.name]: event.target.value })
-//     }
-//     onSubmit(event){
-//         event.preventDefault()
-//         const itemInfo = {
-//             item: this.state.item,
-//             price: this.state.price
-//         }
-//         console.log(itemInfo)
-//     }
-
-//     render() {
-
-//         return (
-//             <div>
-//                 <h1>EXPENSE</h1>
-//                 <form onSubmit={this.onSubmit}>
-//                     <input type='text' name='item' placeholder='item' value={this.state.item} onChange={this.onChange}/>
-//                     <input type='float' name='price' placeholder='price' value={this.state.price} onChange={this.onChange}/>
-//                     <button>Submit</button>
-//                 </form>
-                
-//             </div>
-//         );
-//     }
-// }
 const ExpenseForm = (props)=> {
     const {handleSubmit} = props
     console.log('WORKINGS?')
@@ -75,5 +35,3 @@ const mapDispatch = (dispatch)=>{
     }
 }
 export default connect(mapToState,mapDispatch)(ExpenseForm)
-
-// export default ExpenseForm
